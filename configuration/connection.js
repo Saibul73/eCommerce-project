ac1c10c146300c5b0ad3b2efe0c884bf2dcccf8d
saibul <saibulfariskt3@gmail.com>
2022-11-05 11:40:31 +0530

    ignore update

--- configuration/connection.js
+++ configuration/connection.js
@@ -1,10 +1,10 @@
 
 // const mongoClient = require('mongodb').MongoClient
 const mongoose =require('mongoose');
+const databaseConnect= process.env.MONGO_CONNECT_ID
 
 
-
-    mongoose.connect("mongodb+srv://saibul:Valapil1@urbanbull.bj4exsb.mongodb.net/urbanbull",{useNewUrlParser:true,
+    mongoose.connect(databaseConnect,{useNewUrlParser:true,
     useUnifiedTopology:true
 },(err)=>{
     if(err){
@@ -15,13 +15,3 @@ const mongoose =require('mongoose');
     }
 });
 
-// mongoose.connect("mongodb://localhost:27017/eCommerce",{useNewUrlParser:true,
-// useUnifiedTopology:true
-// },(err)=>{
-// if(err){
-//     console.log('connection err'+err)
-// }
-// else{
-//     console.log('connection established')
-// }
-// });
